4393360d5f24ffe6acffa5627d574f253976804f
Ivan <kii1234@yandex.ru>
2025-05-25 16:56:47 +0300

    3_2 performance test

--- src/main/kotlin/com/kzkv/pso/service/ObstacleService.kt
+++ src/main/kotlin/com/kzkv/pso/service/ObstacleService.kt
@@ -1,11 +1,16 @@
 package com.kzkv.pso.service
 
 import com.kzkv.pso.data.Params
+import com.kzkv.pso.data.Statistic
 import com.kzkv.pso.entity.Obstacle
 import org.springframework.stereotype.Service
 
 @Service
-open class ObstacleService(private val jsonService: JsonService, private val psoService: PsoService) {
+open class ObstacleService(
+	private val jsonService: JsonService,
+	private val psoService: PsoService,
+	private val statisticService: StatisticService
+) {
 
 	fun readObstacles() : List<Obstacle> {
 		psoService.obstacles = jsonService.readObstacles()
@@ -17,6 +22,23 @@ open class ObstacleService(private val jsonService: JsonService, private val pso
 			Obstacle(params.obstacleParams)
 		}
 		return psoService.obstacles
+	}
+
+	private fun createObstacles(params: Params, amount: Int) : List<Obstacle> {
+		psoService.obstacles = List(amount) {
+			Obstacle(params.obstacleParams)
+		}
+		return psoService.obstacles
+	}
+
+	fun test(params: Params) : List<Statistic> {
+		statisticService.clearStats()
+		for (i in 10..1000 step 10) {
+			createObstacles(params, i)
+			psoService.startPSO(params.particleParams)
+			psoService.clearRoute()
+		}
+		return statisticService.writeStatistic()
 	}
 
 }
